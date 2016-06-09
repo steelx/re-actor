98de29243ac9eb13655aaa5082c8ae22df82b4e3
Rodrigo Rigo <dallrigo@gmail.com>
2016-06-09 20:21:47 +0530

    supports sass

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ const common = {
                 exclude: /node_modules/
             },
             {
-                test: /\.scss$/,
+                test: /\.(scss|sass)$/,
                 loader: ExtractTextPlugin.extract('style', ['css', 'postcss', 'sass'])
             },
             {
